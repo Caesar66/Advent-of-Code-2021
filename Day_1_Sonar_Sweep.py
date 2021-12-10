1a48df79b81c874a6a4a79f5a4ea5facfd974a48
César Silva <cesar.augustojs06@gmail.com>
2021-12-09 22:25:11 -0300

    Create Day_1_Sonar_Sweep.py

---
+++ Day_1_Sonar_Sweep.py
@@ -0,0 +1,20 @@
+
+def getIncrementCounter(list_):
+    counter = 0
+    for i in range(1, len(list_)):
+        if list_[i] > list_[i-1]:
+            counter += 1
+    return counter
+
+def getSumIncrementCounter(list_):
+    counter = 0
+    for i in range(len(list_)-3):
+        if (list_[i] + list_[i+1] + list_[i+2] < list_[i+1] + list_[i+2] + list_[i+3]):
+            counter += 1
+    return counter
+
+with open("input.txt", 'r') as depth_file:
+    depths = [int(line.rstrip()) for line in depth_file]
+
+print(getIncrementCounter(depths))
+print(getSumIncrementCounter(depths))
